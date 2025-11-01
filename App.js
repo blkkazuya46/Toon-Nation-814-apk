f446b6e9d3d10dd0334082eb074d6bc1e27b4ba7
blkkazuya46 <blkkazuya46@gmail.com>
2025-11-01 15:09:10 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, Platform, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  const webUrl = Constants?.expoConfig?.extra?.webUrl || 'https://toon-nation-814.onrender.com';
+  const topPad = Platform.OS === 'android' ? StatusBar.currentHeight ?? 0 : 0;
+
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: topPad, backgroundColor: '#000' }}>
+      <WebView
+        source={{ uri: webUrl }}
+        javaScriptEnabled
+        domStorageEnabled
+        startInLoadingState
+        mixedContentMode="always"
+        allowsFullscreenVideo
+        originWhitelist={['*']}
+      />
+    </SafeAreaView>
+  );
+}
