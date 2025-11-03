8cdb356cbf42cd5dacfea72195bd2d4389642fc3
blkkazuya46 <blkkazuya46@gmail.com>
2025-11-03 07:50:56 -0500

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// (optional) keep it vanilla; no extra customizations needed
+module.exports = config;
