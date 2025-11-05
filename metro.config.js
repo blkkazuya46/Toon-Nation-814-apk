7cd97ddc5bcd7278a03ea14d18955445005926de
blkkazuya46 <blkkazuya46@gmail.com>
2025-11-05 01:18:07 -0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,3 @@
-// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
-
 const config = getDefaultConfig(__dirname);
-
-// (optional) keep it vanilla; no extra customizations needed
 module.exports = config;
